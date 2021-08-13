27fd78842f4173fee88c30644a4ac9d08cd22448
Maciek Mielczarek <maciejka-67@o2.pl>
2021-08-14 00:42:26 +0200

    Complete titleClickHandler function. Make it to set clicked link active, to get its href attribute and to find on that basis id of corresponding article. Make this article active as well to highlight it.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 'use strict';
 
 const titleClickHandler = function(e){
+  e.preventDefault();
+  const clickedElement = this;
+  const newActiveArticleId = clickedElement.getAttribute('href');
   const activeLinks = document.querySelectorAll('.titles a.active');
   for (let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
+  clickedElement.classList.add('active')
   const activeArticles = document.querySelectorAll('article.post.active');
   for (let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
-  }  
+  }
+  document.querySelector(newActiveArticleId).classList.add('active')
 }
 
 const links = document.querySelectorAll('.titles a');
