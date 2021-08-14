4dcb2924429fe1f97953bd4ec68800aff9a5d59d
Maciek Mielczarek <maciejka-67@o2.pl>
2021-08-14 16:09:21 +0200

    Define generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,60 @@
 'use strict';
 
+{
+const optActiveClass = 'active';
+
+const optSelectors = {
+  article: '.post',
+  articleTitle: '.post-title',
+  activeArticle: '.post.' + optActiveClass,
+  /*activeArticle: this.article + '.' + optActiveClass,
+  Poproszę o wyjaśnienie, dlaczego powyższy wiersz nie działa. Ja to kiedyś rozumiałem, a teraz pustka w głowie*/
+  titleList: '.titles',
+  titleLinks: '.titles a',
+  activeTitleLink: '.titles a.' + optActiveClass,
+  firstTitleLink: 'li:first-of-type a'
+}
+
+const titleLinkTemplate = {
+  firstPart: '<li><a href=\"#',
+  thirdPart: '\"><span>',
+  fifthPart: '</span></a></li>'
+}
+
+
 const titleClickHandler = function(e){
   e.preventDefault();
   const clickedElement = this;
   const newActiveArticleId = clickedElement.getAttribute('href');
-  const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll(optSelectors.activeTitleLink);
   for (let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+    activeLink.classList.remove(optActiveClass);
   }
-  clickedElement.classList.add('active')
-  const activeArticles = document.querySelectorAll('article.post.active');
+  clickedElement.classList.add(optActiveClass)
+  const activeArticles = document.querySelectorAll(optSelectors.activeArticle);
   for (let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+    activeArticle.classList.remove(optActiveClass);
   }
-  document.querySelector(newActiveArticleId).classList.add('active')
+  document.querySelector(newActiveArticleId).classList.add(optActiveClass)
 }
 
-const links = document.querySelectorAll('.titles a');
+const generateTitleLinks = function(selector){
+  const titleList = document.querySelector(optSelectors.titleList);
+  titleList.innerHTML = '';
+  const listofArticles = document.querySelectorAll(selector);
+  let newTitleListContent = '';
+  for(let article of listofArticles){
+    newTitleListContent += (titleLinkTemplate.firstPart + article.getAttribute('id') +
+    titleLinkTemplate.thirdPart + article.querySelector(optSelectors.articleTitle).textContent +
+    titleLinkTemplate.fifthPart);
+  }
+  titleList.innerHTML = newTitleListContent;
+  titleList.querySelector(optSelectors.firstTitleLink).classList.add(optActiveClass);
+  const links = document.querySelectorAll(optSelectors.titleLinks);
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+}
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+generateTitleLinks(optSelectors.article);
 }
