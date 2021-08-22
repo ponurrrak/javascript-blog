4e3b5a22d12d4b50218747e2cb8ef93f18d5dbe3
Maciek Mielczarek <maciejka-67@o2.pl>
2021-08-22 16:46:07 +0200

    Replace raw HTML strings in JS with Handlebars templates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 'use strict';
 
 {
+  const templates = {
+    articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML),
+    authorLink: Handlebars.compile(document.querySelector('#template-author-link').innerHTML),
+    tagLink: Handlebars.compile(document.querySelector('#template-tag-link').innerHTML),
+    authorsListLink: Handlebars.compile(document.querySelector('#template-authors-list-link').innerHTML),
+  };
+
   const optClassesConfig = {
     activeClassName: 'active',
     cloudClassCount: 5,
@@ -19,9 +26,6 @@
       return `a[href="${link}"]`;
     },
     tagsCloudListWrapper: '.list.tags',
-    articleTagTemplate: function(tag){
-      return `<li><a href="#tag-${tag}">${tag}</a></li>`;
-    },
     cloudTagsLastLink: 'li:last-of-type a',
     authorsListWrapper: '.list.authors'
   };
@@ -62,7 +66,10 @@
     let newTitleListContent = '';
     const listofArticles = selector ? document.querySelectorAll(selector) : fullListofArticles;
     for(let article of listofArticles){
-      newTitleListContent += `<li><a href="#${article.getAttribute('id')}"><span>${article.querySelector(optSelectors.articleTitle).textContent}</span></a></li>`;
+      newTitleListContent += templates.articleLink({
+        id: article.getAttribute('id'),
+        title: article.querySelector(optSelectors.articleTitle).textContent
+      });
     }
     titleList.innerHTML = newTitleListContent;
     const links = document.querySelectorAll(optSelectors.titleLinks);
@@ -93,27 +100,31 @@
   const generateTagsCloud = function(uniqueTagsCounter){
     const tagsParams = calculateTagsParams(uniqueTagsCounter);
     const tagsCloudListWrapper = document.querySelector(optSelectors.tagsCloudListWrapper);
+    const tagsData = {tags: []};
     for(let tag in uniqueTagsCounter){
-      tagsCloudListWrapper.insertAdjacentHTML('beforeend', optSelectors.articleTagTemplate(tag));
-      tagsCloudListWrapper.querySelector(optSelectors.cloudTagsLastLink).classList.add(calculateTagClass(uniqueTagsCounter[tag], tagsParams));
+      tagsData.tags.push({
+        tag: tag,
+        class: calculateTagClass(uniqueTagsCounter[tag], tagsParams)
+      });
     }
+    tagsCloudListWrapper.innerHTML = templates.tagLink(tagsData);
   };
 
   const generateTags = function(){
     const uniqueTagsCounter = {};
     for(let article of fullListofArticles){
       const articleTagsList = article.querySelector(optSelectors.articleTags);
-      let articleTagsListContent = '';
+      const articleTagsListContent = {tags: []};
       const tags = article.getAttribute('data-tags').split(' ');
       for(let tag of tags){
-        articleTagsListContent += optSelectors.articleTagTemplate(tag);
+        articleTagsListContent.tags.push({tag: tag, class: ''});
         if(!uniqueTagsCounter[tag]){
           uniqueTagsCounter[tag] = 1;
         } else {
           uniqueTagsCounter[tag]++;
         }
       }
-      articleTagsList.innerHTML = articleTagsListContent;
+      articleTagsList.innerHTML = templates.tagLink(articleTagsListContent);
     }
     generateTagsCloud(uniqueTagsCounter);
   };
@@ -143,18 +154,21 @@
 
   const generateAuthorsList = function(articlesPerAuthor){
     const authorsListWrapper = document.querySelector(optSelectors.authorsListWrapper);
-    let authorsListHTML = '';
+    const authorsData = {authors: []};
     for(let author in articlesPerAuthor){
-      authorsListHTML += `<li><a href="#author-${author}">${author}</a><span> (${articlesPerAuthor[author]})</span></li>`;
+      authorsData.authors.push({
+        author: author,
+        count: articlesPerAuthor[author]
+      });
     }
-    authorsListWrapper.innerHTML = authorsListHTML;
+    authorsListWrapper.innerHTML = templates.authorsListLink(authorsData);
   };
 
   const generateAuthors = function(){
     const articlesPerAuthor = {};
     for(let article of fullListofArticles){
       const author = article.getAttribute('data-author');
-      article.querySelector(optSelectors.articleAuthor).innerHTML = `<a href="#author-${author}">by ${author}</a>`;
+      article.querySelector(optSelectors.articleAuthor).innerHTML = templates.authorLink({author: author});
       if(!articlesPerAuthor[author]){
         articlesPerAuthor[author] = 1;
       } else {
